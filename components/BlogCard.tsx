86dc3f0528da403db46fd90b8ce47d9b0abbb64f
Sean OReilly <soreilly424@gmail.com>
2023-01-10 11:06:25 -0500

    update text and colors of blogcard

--- components/BlogCard.tsx
+++ components/BlogCard.tsx
@@ -12,7 +12,7 @@ const BlogCard = (props: PostType) => {
     <Link href={`/posts/${id}`}>
       <div
         className={clsx(
-          'grid gap-5 rounded-xl bg-cyan-100 p-5 leading-loose shadow transition-all duration-100 ease-linear dark:bg-slate-800 dark:text-white',
+          'grid gap-5 rounded-xl border-4 border-[#caf0f8] bg-[#90e0ef] p-5 leading-loose shadow transition-all duration-100 ease-linear dark:border-[#caf0f8] dark:bg-[#03045e] dark:text-[#caf0f8]',
           inter.className
         )}
       >
@@ -21,13 +21,15 @@ const BlogCard = (props: PostType) => {
             tags.map((tag, i) => (
               <li
                 key={i}
-                className="rounded-full border border-black px-4 py-1 uppercase"
+                className={clsx(
+                  'rounded-full border-2 border-white bg-[#caf0f8] px-4 py-1 text-xs font-black uppercase dark:bg-[#caf0f8] dark:text-black'
+                )}
               >
                 {tag}
               </li>
             ))}
         </ul>
-        <h4 className="text-xl font-black md:text-2xl lg:text-3xl">{title}</h4>
+        <h4 className="text-xl font-black md:text-3xl lg:text-4xl">{title}</h4>
         <Date dateString={date ?? ''} />
       </div>
     </Link>

bdf6c5ebf361fe9da1dcf4ef0199760b26890f56
Sean OReilly <soreilly424@gmail.com>
2023-01-10 17:12:02 -0500

    adding sanity

---
+++ studio/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'n93acrcs',
+    dataset: 'production'
+  }
+})
---
+++ studio/schemas/post.js
@@ -0,0 +1,12 @@
+export default {
+  name: 'post',
+  type: 'document',
+	title: 'Post',
+  fields: [
+    {
+      name: 'name',
+      type: 'string',
+      title: 'Name'
+    }
+  ]
+}

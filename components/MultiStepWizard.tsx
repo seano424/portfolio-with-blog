7995fd0c65e08e60636b30039a7173e66b542e97
Sean OReilly <soreilly424@gmail.com>
2023-01-11 12:57:56 -0500

    adding transition for checks

--- components/MultiStepWizard.tsx
+++ components/MultiStepWizard.tsx
@@ -37,20 +37,7 @@ function Step({ number }: { number: number }) {
     >
       <span className="flex items-center justify-center">
         {status === 'complete' ? (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="h-16 w-16 text-white"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-            />
-          </svg>
+          <CheckIcon className="h-6 w-6 text-white" />
         ) : (
           <span>{number}</span>
         )}
@@ -99,5 +86,26 @@ export default function MultiStepWizard() {
         </div>
       </div>
     </div>
+  )
+}
+
+function CheckIcon(props: any) {
+  return (
+    <svg
+      {...props}
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth={3}
+    >
+      <motion.path
+        initial={{ pathLength: 0 }}
+        animate={{ pathLength: 1 }}
+        // transition={{ duration: 2 }}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M5 13l4 4L19 7"
+      />
+    </svg>
   )
 }

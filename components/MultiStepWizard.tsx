f3fb31f984a30118234e5169cc893e2949aed042
Sean OReilly <soreilly424@gmail.com>
2023-01-11 12:50:23 -0500

    cleaning up step animation

--- components/MultiStepWizard.tsx
+++ components/MultiStepWizard.tsx
@@ -1,74 +1,74 @@
-import { useState } from 'react'
 import { motion } from 'framer-motion'
+import { useAtom, atom } from 'jotai'
 import clsx from 'clsx'
 
-function Step({
-  step,
-  currentStep,
-  setStep,
-}: {
-  step: number
-  currentStep: number
-  setStep: any
-}) {
+export const stepAtom = atom(1)
+
+function Step({ number }: { number: number }) {
+  const [step, setStep] = useAtom(stepAtom)
+
   let status =
-    currentStep === step
-      ? 'active'
-      : currentStep < step
-      ? 'inactive'
-      : 'complete'
+    step === number ? 'active' : step < number ? 'inactive' : 'complete'
 
   return (
     <motion.button
-      onClick={() => setStep(step + 1)}
-      animate={{
-        backgroundColor: status === 'complete' ? 'var(--blue-500)' : '#fff',
-        borderColor:
-          status === 'complete' || status === 'active'
-            ? 'var(--blue-500)'
-            : 'var(--slate-200)',
-        color:
-          status === 'complete' || status === 'active'
-            ? 'var(--blue-500)'
-            : 'var(--slate-400)',
-      }}
+      initial={false}
+      animate={status}
       transition={{ duration: 1 }}
+      variants={{
+        inactive: {
+          backgroundColor: 'var(--white)',
+          borderColor: 'var(--slate-200)',
+          color: 'var(--slate-400)',
+        },
+        active: {
+          backgroundColor: 'var(--white)',
+          borderColor: 'var(--blue-500)',
+          color: 'var(--blue-500)',
+        },
+        complete: {
+          backgroundColor: 'var(--blue-500)',
+          borderColor: 'var(--blue-500)',
+          color: 'var(--blue-500)',
+        },
+      }}
+      onClick={() => setStep(number + 1)}
       className="flex h-10 w-10 items-center justify-center rounded-full border-2 font-semibold"
     >
-      <div className="flex items-center justify-center">
+      <span className="flex items-center justify-center">
         {status === 'complete' ? (
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
             className="h-16 w-16 text-white"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M9 12.75L11.25 15 15 9.75M21"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
             />
           </svg>
         ) : (
-          <span>{step}</span>
+          <span>{number}</span>
         )}
-      </div>
+      </span>
     </motion.button>
   )
 }
 
 export default function MultiStepWizard() {
-  const [step, setStep] = useState(1)
+  const [step, setStep] = useAtom(stepAtom)
 
   return (
     <div className="my-10 mx-auto w-full max-w-md rounded-2xl bg-tahiti-100 shadow-md">
       <div className="flex justify-between rounded p-8">
-        <Step step={1} setStep={setStep} currentStep={step} />
-        <Step step={2} setStep={setStep} currentStep={step} />
-        <Step step={3} setStep={setStep} currentStep={step} />
-        <Step step={4} setStep={setStep} currentStep={step} />
+        <Step number={1} />
+        <Step number={2} />
+        <Step number={3} />
+        <Step number={4} />
       </div>
       <div className="px-8 pb-8">
         <div>

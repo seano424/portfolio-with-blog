62eb45f03966f3daf72f2880101b4aa6a695b166
Sean OReilly <soreilly424@gmail.com>
2023-01-12 09:28:29 -0500

    wrapping wizard

--- components/MultiStepWizard.tsx
+++ components/MultiStepWizard.tsx
@@ -10,38 +10,60 @@ function Step({ number }: { number: number }) {
     step === number ? 'active' : step < number ? 'inactive' : 'complete'
 
   return (
-    <motion.button
-      initial={false}
-      animate={status}
-      transition={{ duration: 1 }}
-      variants={{
-        inactive: {
-          backgroundColor: 'var(--white)',
-          borderColor: 'var(--slate-200)',
-          color: 'var(--slate-400)',
-        },
-        active: {
-          backgroundColor: 'var(--white)',
-          borderColor: 'var(--blue-500)',
-          color: 'var(--blue-500)',
-        },
-        complete: {
-          backgroundColor: 'var(--blue-500)',
-          borderColor: 'var(--blue-500)',
-          color: 'var(--blue-500)',
-        },
-      }}
-      onClick={() => setStep(number + 1)}
-      className="flex h-10 w-10 items-center justify-center rounded-full border-2 font-semibold"
-    >
-      <span className="flex items-center justify-center">
-        {status === 'complete' ? (
-          <CheckIcon className="h-6 w-6 text-white" />
-        ) : (
-          <span>{number}</span>
-        )}
-      </span>
-    </motion.button>
+    <motion.div animate={status} className="relative">
+      <motion.div
+        variants={{
+          active: {
+            scale: 1,
+            transition: {
+              delay: 0,
+              duration: 0.3,
+            },
+          },
+          complete: {
+            scale: 1.25,
+          },
+        }}
+        transition={{
+          duration: 0.5,
+          delay: 0.2,
+          type: 'tween',
+          ease: 'circOut',
+        }}
+        className="absolute inset-0  rounded-full bg-blue-200"
+      ></motion.div>
+      <motion.button
+        onClick={() => setStep(number + 1)}
+        initial={false}
+        variants={{
+          inactive: {
+            backgroundColor: 'var(--white)',
+            borderColor: 'var(--slate-200)',
+            color: 'var(--slate-400)',
+          },
+          active: {
+            backgroundColor: 'var(--white)',
+            borderColor: 'var(--blue-500)',
+            color: 'var(--blue-500)',
+          },
+          complete: {
+            backgroundColor: 'var(--blue-500)',
+            borderColor: 'var(--blue-500)',
+            color: 'var(--blue-500)',
+          },
+        }}
+        transition={{ duration: 0.2 }}
+        className="relative flex h-10 w-10 items-center justify-center rounded-full border-2 font-semibold"
+      >
+        <span className="flex items-center justify-center">
+          {status === 'complete' ? (
+            <CheckIcon className="h-6 w-6 text-white" />
+          ) : (
+            <span>{number}</span>
+          )}
+        </span>
+      </motion.button>
+    </motion.div>
   )
 }
 
@@ -100,7 +122,12 @@ function CheckIcon(props: any) {
       <motion.path
         initial={{ pathLength: 0 }}
         animate={{ pathLength: 1 }}
-        // transition={{ duration: 2 }}
+        transition={{
+          delay: 0.2,
+          type: 'tween',
+          ease: 'easeOut',
+          duration: 0.3,
+        }}
         strokeLinecap="round"
         strokeLinejoin="round"
         d="M5 13l4 4L19 7"

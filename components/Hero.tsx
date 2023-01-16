e2b29683de079de25d0f34d042ab6724d834185e
Sean OReilly <soreilly424@gmail.com>
2023-01-16 11:01:40 -0500

    adding hero image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import GitHub from '@/icons/GitHub'
 import Twitter from '@/icons/Twitter'
 import LinkedIn from '@/icons/LinkedIn'
+import Image from 'next/image'
 
 const socialLinks = [
   {
@@ -29,29 +30,40 @@ const socialLinks = [
 
 export default function Hero() {
   return (
-    <div className="pb-10">
-      <div className="flex justify-center gap-8">
-        {socialLinks.map((link) => (
-          <Link
-            href={link.href}
-            key={link.name}
-            className={link.classes}
-            aria-label={link.name}
-            target="_blank"
-            rel="noreferrer"
-          >
-            {link.icon === 'GitHub' && (
-              <GitHub className="h-8 w-8 text-green-500 transition-all duration-300 ease-linear hover:text-primary-pink dark:text-green-200 dark:hover:text-primary-pink" />
-            )}
-            {link.icon === 'Twitter' && (
-              <Twitter className="h-8 w-8 text-blue-500 transition-all duration-200 ease-linear hover:text-primary-pink dark:text-primary-blue dark:hover:text-white" />
-            )}
-            {link.icon === 'LinkedIn' && (
-              <LinkedIn className="h-8 w-8 text-primary-pink transition-all duration-700 ease-linear hover:text-primary-blue dark:text-blue-200 dark:hover:text-white" />
-            )}
-          </Link>
-        ))}
+    <section className="flex flex-col items-center gap-10">
+      <div className="relative h-64 w-64 rounded-full border-8 border-primary-300 transition-all duration-200 ease-in-out dark:border-white">
+        <Image
+          className="rounded-full object-cover"
+          src="/images/me.png"
+          alt="Personal Photo"
+          priority
+          fill
+        />
       </div>
-    </div>
+      <div className="pb-10">
+        <div className="flex justify-center gap-8">
+          {socialLinks.map((link) => (
+            <Link
+              href={link.href}
+              key={link.name}
+              className={link.classes}
+              aria-label={link.name}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {link.icon === 'GitHub' && (
+                <GitHub className="h-8 w-8 text-green-500 transition-all duration-300 ease-linear hover:text-primary-pink dark:text-green-200 dark:hover:text-primary-pink" />
+              )}
+              {link.icon === 'Twitter' && (
+                <Twitter className="h-8 w-8 text-blue-500 transition-all duration-200 ease-linear hover:text-primary-pink dark:text-primary-blue dark:hover:text-white" />
+              )}
+              {link.icon === 'LinkedIn' && (
+                <LinkedIn className="h-8 w-8 text-primary-pink transition-all duration-700 ease-linear hover:text-primary-blue dark:text-blue-200 dark:hover:text-white" />
+              )}
+            </Link>
+          ))}
+        </div>
+      </div>
+    </section>
   )
 }

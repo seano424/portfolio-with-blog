58822eb6e926a6510c5c60233d3f6e71aabd7c37
Sean OReilly <soreilly424@gmail.com>
2023-01-17 10:40:13 -0500

    adjusting articles page spacing

--- pages/articles/[id].tsx
+++ pages/articles/[id].tsx
@@ -13,11 +13,11 @@ const Post = ({ post }: InferGetStaticPropsType<typeof getStaticProps>) => {
   const { back } = useRouter()
 
   return (
-    <div className="container pt-10 dark:text-white lg:pt-20">
+    <div className="pl-base lg:pl-auto container pt-5 dark:text-white lg:pt-20">
       <div className="grid gap-3">
         <button
           onClick={() => back()}
-          className="group w-max transform rounded-3xl bg-dark px-4 py-3 transition-all duration-200 ease-linear hover:scale-105 dark:bg-light"
+          className="group mb-5 w-max transform rounded-3xl bg-dark px-4 py-3 transition-all duration-200 ease-linear hover:scale-105 dark:bg-light"
           aria-label="Go back button"
         >
           <ArrowLeftIcon className="h-4 w-4 transform text-light transition-all duration-200 ease-linear group-hover:scale-105 dark:text-dark" />

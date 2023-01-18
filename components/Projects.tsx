9ef4a3217234c74df349e31c7fc83c1588ab618e
Sean OReilly <soreilly424@gmail.com>
2023-01-18 09:22:05 -0500

    add projects page

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -5,7 +5,7 @@ import GitHub from '@/icons/GitHub'
 
 export default function Projects() {
   return (
-    <section className="pl-base lg:pl-auto container my-24">
+    <section className="pl-base lg:pl-auto container py-10">
       <h2 className="text-5xl font-black uppercase tracking-tighter text-primary-900 sm:text-7xl">
         <span className="bg-gradient-to-r from-primary-900 to-primary-blue bg-clip-text text-transparent dark:from-primary-100 dark:to-primary-700">
           Recent{' '}

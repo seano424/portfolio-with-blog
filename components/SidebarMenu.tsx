ab5895d5ee047dd41f5844d82543f40ff25ae6fd
Sean OReilly <soreilly424@gmail.com>
2023-01-18 11:52:55 -0500

    updating sidebar colors

--- components/SidebarMenu.tsx
+++ components/SidebarMenu.tsx
@@ -67,25 +67,25 @@ export default function SidebarMenu() {
           className="fixed z-30 flex h-full min-h-screen w-full flex-col gap-8 lg:hidden"
         >
           <div className="absolute inset-0 shadow-2xl filter backdrop-blur"></div>
-          <div className="container absolute inset-0 flex flex-col gap-10  pt-40">
+          <div className="container absolute inset-0 flex flex-col gap-10 bg-white/90 pt-40 dark:bg-white/0">
             {mobileNavLinks.map((link, i) => (
               <a
                 onClick={(e) => handleClick(e, link.href)}
                 key={link.title}
                 href={`/${link.href}`}
                 className={clsx(
-                  i === 0 && 'duration-200',
-                  i === 1 && 'duration-300',
-                  i === 2 && 'duration-500',
-                  i === 3 && 'duration-300',
-                  'text-6xl font-black tracking-tighter text-black transition-all ease-linear hover:text-primary-pink dark:text-white dark:hover:text-cyan-300 sm:text-8xl'
+                  i === 0 && 'duration-150',
+                  i === 1 && 'duration-200',
+                  i === 2 && 'duration-300',
+                  i === 3 && 'duration-500',
+                  'text-6xl font-black tracking-tighter text-dark transition-all ease-linear hover:text-fuchsia-500 dark:text-white dark:drop-shadow-2xl dark:hover:text-cyan-300 sm:text-8xl'
                 )}
               >
                 {link.title}
               </a>
             ))}
             <Link
-              className="text-6xl font-black tracking-tighter text-black transition-all duration-500 ease-linear hover:text-primary-pink dark:text-white dark:hover:text-cyan-300 sm:text-8xl"
+              className="text-6xl font-black tracking-tighter text-dark transition-all duration-700 ease-linear hover:text-fuchsia-500 dark:text-white dark:drop-shadow-2xl dark:hover:text-cyan-300 sm:text-8xl"
               href="mailto:soreilly424@gmail.com"
               target="_blank"
               rel="noreferrer noopener"

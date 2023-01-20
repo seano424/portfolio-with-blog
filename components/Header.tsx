9bc2351ad49e71e53c43be633ea1d28797e83cd4
Sean OReilly <soreilly424@gmail.com>
2023-01-20 11:23:11 -0500

    updating hover colors on header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,10 @@ import Link from 'next/link'
 import Image from 'next/image'
 import MenuIcon from './MenuIcon'
 import ThemeButton from './ThemeButton'
+import { useScroll, motion } from 'framer-motion'
+import { useRouter } from 'next/router'
+import clsx from 'clsx'
+import { useEffect, useState } from 'react'
 
 const links = [
   {
@@ -23,8 +27,31 @@ const links = [
 ]
 
 export default function Header() {
+  const { scrollYProgress } = useScroll()
+  const [scrollPosition, setScrollPosition] = useState(0)
+  const router = useRouter()
+
+  const handleScroll = () => {
+    const position = window.pageYOffset
+    setScrollPosition(position)
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll, { passive: true })
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [scrollYProgress])
+
   return (
-    <nav className="fixed z-40 flex h-20 w-full bg-light bg-transparent bg-opacity-90 py-4 filter backdrop-blur-sm transition-all duration-75 ease-linear dark:bg-transparent dark:bg-dark">
+    <motion.nav className="fixed z-40 flex h-20 w-full bg-light bg-transparent bg-opacity-90 py-4 filter backdrop-blur-sm transition-all duration-200 ease-linear dark:bg-transparent">
+      {router.pathname === '/articles' && (
+        <motion.div
+          style={{ scaleX: scrollYProgress }}
+          className="absolute left-0 right-0 top-0 h-4 origin-left transform rounded-br-3xl bg-primary-100 lg:hidden"
+        ></motion.div>
+      )}
       <div className="container flex items-center justify-between">
         <Link
           href="/"
@@ -47,7 +74,12 @@ export default function Header() {
             <li key={link.title}>
               <Link
                 href={`/${link.href}`}
-                className="button transform rounded-full px-5 py-4 text-3xl font-black tracking-tighter transition-all  duration-700 ease-linear hover:bg-gray-200/60 dark:text-light dark:hover:scale-110 dark:hover:bg-neutral-900/50 dark:hover:text-gray-100"
+                className={clsx(
+                  'button transform rounded-full px-5 py-4 text-3xl font-black tracking-tighter transition-all  duration-500 ease-linear hover:bg-gray-200/60 dark:text-light dark:hover:scale-110 dark:hover:text-gray-100',
+                  scrollPosition > 140
+                    ? 'dark:hover:bg-neutral-900/50'
+                    : 'dark:hover:bg-light/30'
+                )}
               >
                 {link.title}
               </Link>
@@ -57,6 +89,6 @@ export default function Header() {
         <MenuIcon />
         <ThemeButton />
       </div>
-    </nav>
+    </motion.nav>
   )
 }
